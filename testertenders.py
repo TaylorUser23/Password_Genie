bc907d66ff6b718ea308246cb42c5633e9c77b64
ITSupportTaylor <110524028+TaylorUser23@users.noreply.github.com>
2023-08-29 09:41:47 -0500

    Add files via upload

--- testertenders.py
+++ testertenders.py
@@ -2,33 +2,38 @@ import random
 import datetime
 
 def generate_password(length=13):
+    #The available characters, numbers, and spec chars
     characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     numbers = "0123456789"
     special_characters = "!@#$%*_~"
-
+    
+    #Adding chars and numbers first
     all_characters = characters + numbers
 
-
+    #joining random numbers and chars for length in range 
     password = ''.join(random.choice(all_characters) for _ in range(length))
-
+    #adding one spec char at the end of my chars and numbs
     g = password + random.choice(special_characters)
 
     return g
 
- 
+#take user input for length of char,num,spec char
 password_length = int(input("Enter the desired password length: ")) - 1
-
+#calling my function
 generated_password = generate_password(password_length)
 print("Generated Password:", generated_password)        
 
+#printing password to this file path
 file_path = r'C:\Users\Taylor\Desktop\Passwords.txt'
-
+#file path must be changed to a path specific to your machine
+#getting date and time from library
 v= str(datetime.datetime.now())
-
+#writing password and time to the file path mentioned earlier
 with open(file_path, 'w') as file:
     file.write(generated_password + '\n')
     file.write(v)
 print("Your password has been saved to this file:" , file_path)
-#figure out how to make the output look cleaner like a string, prob check the a and b variable and try those as strings
-#also, try print it to a text doc somewhere eveytime it makes a password
+
+
+
 #print(dir(random))
